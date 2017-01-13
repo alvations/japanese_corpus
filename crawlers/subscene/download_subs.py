a10390a99a0ae7ef035a9475944b7a45402ca93f
rapigan <rapigan@gmail.com>
2017-01-13 06:32:44 -0800

    sub dl second pass. seems to work alright

--- crawlers/subscene/download_subs.py
+++ crawlers/subscene/download_subs.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 import os
 import time
 from tqdm import tqdm
+import subprocess, shlex
 
 url_base = lambda url: re.findall("https://subscene.com/subtitles/(.*)/[japanese|english]", url)[0]
 
@@ -26,30 +27,43 @@ for url in en_urls:
 
 
 os.system('mkdir ~/Desktop/subs')
-for item in tqdm(list(set(jp_dict.keys()) & set(en_dict.keys()))):
-    os.system('mkdir ~/Desktop/subs/%s' % item)
-    os.system('mkdir ~/Desktop/subs/%s/jp' % item)
-    os.system('mkdir ~/Desktop/subs/%s/en' % item)
+try: 
+    for item in tqdm(list(set(jp_dict.keys()) & set(en_dict.keys()))):
+        os.system('mkdir ~/Desktop/subs/%s' % item)
+        os.system('mkdir ~/Desktop/subs/%s/jp' % item)
+        os.system('mkdir ~/Desktop/subs/%s/en' % item)
 
-    for jp_sub in set(jp_dict[item]):   # set to rm dups
-        os.system('wget %s -O tmp' % jp_sub)
-        os.system('grep "/subtitle/download?" tmp > tmp2')
-        url = open('tmp2').read().strip()
-        url = re.findall('"(.*?)"', url)[0]
-        url = 'https://subscene.com' + url
-        open_new_tab(url)
-    time.sleep(2)
-    os.system('mv ~/Downloads/* ~/Desktop/subs/%s/jp' % item)
+        for jp_sub in set(jp_dict[item]):   # set to rm dups
+            os.system('wget %s -O tmp' % jp_sub)
+            os.system('grep "/subtitle/download?" tmp > tmp2')
+            url = open('tmp2').read().strip()
+            url = re.findall('"(.*?)"', url)[0]
+            url = 'https://subscene.com' + url
+            open_new_tab(url)
+        time.sleep(3)
+        os.system('find ~/Downloads/ -name "*.zip" -exec unzip -o -d ~/Desktop/subs/%s/jp {} \;' % item)
+        os.system('find ~/Downloads/ -name "*.rar" -exec unrar x {} ~/Desktop/subs/%s/jp \;' % item)
+        time.sleep(2)
+        os.system('rm ~/Downloads/*.zip ~/Downloads/*.rar ~/Downloads/*.txt')
+
+        for en_sub in set(en_dict[item]):   # set to rm dups
+            os.system('wget %s -O tmp' % en_sub)
+            os.system('grep "/subtitle/download?" tmp > tmp2')
+            url = open('tmp2').read().strip()
+            url = re.findall('"(.*?)"', url)[0]
+            url = 'https://subscene.com' + url
+            open_new_tab(url)
+        time.sleep(3)
+        os.system('find ~/Downloads/ -name "*.zip" -exec unzip -o -d ~/Desktop/subs/%s/en {} \;' % item)
+        os.system('find ~/Downloads/ -name "*.rar" -exec unrar x {} ~/Desktop/subs/%s/en \;' % item)
+        time.sleep(2)
+        os.system('rm ~/Downloads/*.zip ~/Downloads/*.rar ~/Downloads/*.txt')
+        
 
 
-    for en_sub in set(en_dict[item]):   # set to rm dups
-        os.system('wget %s -O tmp' % en_sub)
-        os.system('grep "/subtitle/download?" tmp > tmp2')
-        url = open('tmp2').read().strip()
-        url = re.findall('"(.*?)"', url)[0]
-        url = 'https://subscene.com' + url
-        open_new_tab(url)
-    time.sleep(2)
-    os.system('mv ~/Downloads/* ~/Desktop/subs/%s/en' % item)
+except KeyboardInterrupt:
+    print 'stopped'
 
-
+finally:
+    # cleanup 
+    os.system('rm tmp tmp2 wget-log')
