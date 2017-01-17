b62496da53d1eca2b265e19c6ee9debe08ac26e0
rapigan <rapigan@gmail.com>
2017-01-17 06:01:42 -0800

    parsing started

---
+++ parsers/subscene/parse_subfile.py
@@ -0,0 +1,12 @@
+"""
+Many subtitle files have weird ass encodings, and aren't nessicarily in srt format.
+  This file should be able to handle pretty much any case, and make a nicely packaged
+  data structure for subs files.
+"""
+
+
+
+
+
+
+
