d7d981b6198939777c3ae03f88847fa9182d1340
rapigan <rapigan@gmail.com>
2017-01-17 10:17:46 -0800

    multithreading. todo: documentaiton

--- parsers/subscene/correct_encodings.py
+++ parsers/subscene/correct_encodings.py
@@ -16,20 +16,11 @@ $ chardetect the_pianist_disk1.srt
 iconv -f SHIFT-JIS -t UTF-8 the_pianist_disk1.srt > out.txt
 
 
-
+TODO: DOCUMENTAATION
 
 RIGHT NOW RUN WITH
 find ~/Documents/japanese_corpus/ -type f | python correct_encodings.py
-
-
-
-
-
-
-
 """
-
-
 import sys
 import os
 from tqdm import tqdm
@@ -43,33 +34,25 @@ def shellquote(s):
     return "'" + s.replace("'", "'\\''") + "'"
 
 def correct(fp):
-#    fp = shellquote(fp)
-#    os.system('chardetect "%s"' % fp)
     output = os.popen('chardetect "%s"' % fp).read()
     # ugly but chardetect's output is determanistic and meh
     charset = output.split(':')[1].strip().split(' ')[0]
-    print charset
     if charset.upper() in supported_encodings:
-        print 'iconv -f %s -t UTF-8 "%s" > "%s"' % (charset, fp, fp + '.utf8')
-        os.system('iconv -f %s -t UTF-8 "%s" > "%s"' % (charset, fp, fp + '.utf8'))
-        print 'CONVERTED \n\t to "%s" \n\t encoding was: %s' % ('utf8_' + fp, charset)
+        os.system('iconv -f %s -t UTF-8 "%s" > "%s"' % (charset, fp, fp))
+        print 'CONVERTED \n\t to "%s" \n\t encoding was: %s' % (fp, charset)
     else:
         print 'SKIPPED \n\t %s \n\t encoding was: %s' % (fp, charset)
 
-MAX_PROCESSES = 4
-processes = set()
-files = [line.strip() for line in sys.stdin]
-#os.system('ls "/Users/rapigan/Documents/japanese_corpus/subs/oceans-thirteen/en/Ocean\'s 13 BRRip-720p[Dual-Audio][Hindi-Eng]-RedHeart.srt"')
-#os.system('chardetect "/Users/rapigan/Documents/japanese_corpus/subs/oceans-thirteen/en/Ocean\'s 13 BRRip-720p[Dual-Audio][Hindi-Eng]-RedHeart.srt"')
-a = "/Users/rapigan/Documents/japanese_corpus/subs/oceans-thirteen/en/Ocean's 13 BRRip-720p[Dual-Audio][Hindi-Eng]-RedHeart.srt"
-correct(a)
-quit()
 
+if __name__ == '__main__':
+    max_proc = 4
+    procs = set()
+    files = [line.strip() for line in sys.stdin]
 
-for file in tqdm(files):
-    p = Process(target=correct, args=(file,))
-    p.start()
-    processes.add(p)
-    if len(processes) >= MAX_PROCESSES:
-        os.wait()
-        processes.difference_update([p for p in processes if p.is_alive()])
+    for file in tqdm(files):
+        p = Process(target=correct, args=(file,))
+        p.start()
+        procs.add(p)
+        while len(procs) >= max_proc:
+            procs.difference_update([p for p in procs if not p.is_alive()])
+
