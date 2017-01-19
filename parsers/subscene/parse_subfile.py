5a542c9a6244e986739ff5d3821653455e343f97
rapigan <rapigan@gmail.com>
2017-01-19 06:35:34 -0800

    en parse done

--- parsers/subscene/parse_subfile.py
+++ parsers/subscene/parse_subfile.py
@@ -17,55 +17,62 @@ Subs to be organized in a directory structure looking like this:
            subs
       ... 
 
+
+
 """
 import sys
 import os
-import collections
+from collections import defaultdict
 import numpy as np
+import re
 
-def distance(source, target):
-    """FAST Levenschtein distance between two strings
-       Used for fuzzy matching of subs to titles
-       
-       might not need!
+
+SRT_TS_PATTERN = '\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}'
+
+
+def clean_ts(x):
+    """cut off timestamps at the second mark, pack start & end in a tuple"""
+    return re.findall('(\d{2}:\d{2}:\d{2}),\d{3} --> (\d{2}:\d{2}:\d{2}),\d{3}', x)[0]
+
+def clean_text(x):
+    """ filter and clean text"""
+    x = x.strip()                     # rm leading and trailing newlines, etc
+    x = re.sub('\d+$', '', x)         # remove srt sub index 
+    x = x.strip()                     # chop off newly exposed newlines 
+    x = re.sub('\r\n|\r|\n', ' ', x)  # join multiline subs
+    return x
+
+
+def parse_en_subfile(file_path):
+    """ makes { (start time, end time) : sub }  mmappings 
     """
-    if len(source) < len(target):
-        return distance(target, source)
+    # .srt accounts for 95.3% (~35k files) of subs in this corpus, so i'm goign to worry about those later
+    _, ext = os.path.splitext(file_path)
+    if ext.lower() != '.srt':
+        return
+    print file_path
+    f = open(os.path.abspath(file_path)).read()
+    
+    timestamps = re.findall(SRT_TS_PATTERN, f)
+    captions = re.split(SRT_TS_PATTERN, f)
+    captions = captions[1:]     # ignore whatever's before the first timestamp
 
-    if len(target) == 0:
-        return len(source)
-
-    # make np character arrays
-    source = np.array(tuple(source))
-    target = np.array(tuple(target))
-
-    # use normal dp algorithm. throw away everything but last two rows of matrix
-    previous_row = np.arange(target.size + 1)
-    for s in source:
-        # insertion (cost of 1)
-        current_row = previous_row + 1
-
-        # substitution (cost of 1) or matching (cost of 0)
-        current_row[1:] = np.minimum(
-                current_row[1:],
-                np.add(previous_row[:-1], target != s))
-
-        # deletion (cost of 1)
-        current_row[1:] = np.minimum(
-                current_row[1:],
-                current_row[0:-1] + 1)
-
-        previous_row = current_row
-
-    return previous_row[-1]
+    return {clean_ts(ts): clean_text(txt) for (ts, txt) in zip(timestamps, captions)}
 
 
 root = sys.argv[1]
 
 # {movie title: subs for that title}  mapping
-SUBS = {title: {'en': [], 'jp': []} for title in os.listdir(root)}
+SUBS = {title: {'en': {}, 'jp': {}} for title in os.listdir(root)}
+
+filetypes = defaultdict(lambda: 0)
 
 
 for title in SUBS:
-    print os.listdir(root + '/' + title + '/en/')
-    print os.listdir(root + '/' + title + '/jp/')
+    for en_sub in os.listdir(root + '/' + title + '/en/'):
+        parse_subfile('%s/%s/en/%s' % (root, title, en_sub))
+
+#    for jp_sub in os.listdir(root + '/' + title + '/jp/'):
+
+
+
