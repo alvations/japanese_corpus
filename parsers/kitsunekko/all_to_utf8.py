a55409fc369ef0f10d958eeccf2e40a3212433f1
rapigan <rapigan@gmail.com>
2017-01-30 06:16:31 -0800

    more encoding conversion refactor

--- parsers/kitsunekko/all_to_utf8.py
+++ parsers/kitsunekko/all_to_utf8.py
@@ -2,6 +2,9 @@
 Many subtitle files have weird ass encodings like SHIFT-JIS. 
 This python script replaces every file it's piped with good 'ole UTF-8 .
 
+The biggest problem with this script is that chardetect can't 
+
+
 === EXAMPLE USAGE: recursively converts everything in given directory:
 find ~/Dir/with/files/ -type f | python all_to_utf8.py
 
@@ -11,9 +14,6 @@ import os
 from tqdm import tqdm
 from multiprocessing import Process
 
-
-# hacky but meh
-supported_encodings = [w.upper() for l in open('supported_encodings.txt').readlines() for w in l.strip().split()]
 
 def is_supported(filename, encodings):
     file_encoding = os.popen('chardetect "%s"' % filename).read()
@@ -26,16 +26,23 @@ def to_utf8(fp, encodings):
     def get_charset(fp):
         """ detect encoding of a file """
         output = os.popen('chardetect "%s"' % fp).read()
+        print 'HERE', output
+        print fp
+        print '\n\n\n'
         charset = output.split(':')[1].strip().split(' ')[0]
         return charset.upper()
 
-    # if that character set is supported by iconv, convert
-    if get_charset(fp) in encodings:
-        os.system('iconv -f %s -t UTF-8 "%s" > "%s"' % (charset, fp, fp + '.utf8'))
-        os.system('mv "%s" "%s"' % (fp + '.utf8', fp))
-        print 'CONVERTED \n\t to "%s" \n\t encoding was: %s' % (fp, charset)
-    else:
-        print 'SKIPPED \n\t %s \n\t encoding was: %s' % (fp, charset)
+    # if that character set is supported by iconv, convert it
+    try:
+        charset = get_charset(fp)
+        if charset in encodings:
+            os.system('iconv -f %s -t UTF-8 "%s" > "%s"' % (charset, fp, fp + '.utf8'))
+            os.system('mv "%s" "%s"' % (fp + '.utf8', fp))
+            print 'CONVERTED \n\t to "%s" \n\t encoding was: %s' % (fp, charset)
+        else:
+            raise Exception
+    except Exception:
+        print 'SKIPPED \n\t %s' % fp
 
 
 if __name__ == '__main__':
@@ -46,13 +53,13 @@ if __name__ == '__main__':
     # run concurrently 
     MAX_PROCESSES = 4
     processes = set()
-    files = [line.strip() for line in sys.stdin]
+    files = [line.strip() for line in sys.stdin if 'DS_Store' not in line]
 
     for file in tqdm(files):
         p = Process(target=to_utf8, args=(file, supported_encodings,))
         p.start()
         processes.add(p)
-        while len(procs) >= MAX_PROCESSES:
+        while len(processes) >= MAX_PROCESSES:
             processes.difference_update([p for p in processes if not p.is_alive()])
 
     # cleanup
