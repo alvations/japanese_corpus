691e4c33c73f401fce39c456f67f8600c78d267f
rapigan <rapigan@gmail.com>
2017-02-13 06:56:41 -0800

    link resolution complete

--- crawlers/daddicts/reids_attempt/count_matching_titles.py
+++ crawlers/daddicts/reids_attempt/count_matching_titles.py
@@ -9,7 +9,12 @@ python count_matching_titles.py en_links.txt jp_links.txt
 import sys
 import re
 from fuzzywuzzy import fuzz
+import os
+import json
 
+#########
+#### (1) : download title listing and do soft intersection of jp/en titles
+#########
 
 def extract_title(s):
     """ extracts title from a line like
@@ -37,12 +42,54 @@ extra = sorted([(similarity(en, jp), (en, jp)) for en in en_remainder for jp in 
 extra = extra[:150]
 
 en_matches = list(matches)[:]
-jp_matches = list(matches)[:]
-
 en_matches += [en for (s, (en, jp)) in extra]
+en_matches = [x.lower() for x in en_matches]
+
+jp_matches = list(matches)[:]
 jp_matches += [jp for (s, (en, jp)) in extra]
+jp_matches = [x.lower() for x in jp_matches]
 
 
 
+#########
+###  (2) : download denny's parsings and resolve my title list with these
+#########
+
+def get_dl_links(title, links):
+    if title in links.keys():
+        return links[title]
+    else:
+        key = max([(similarity(title, k), k) for k in links.keys()])[1]
+        return links[key]
+
+#os.system('wget https://raw.githubusercontent.com/rpryzant/japanese_corpus/master/crawlers/daddicts/download_links_en.jsonlines')
+#os.system('wget https://raw.githubusercontent.com/rpryzant/japanese_corpus/master/crawlers/daddicts/download_links_ja.jsonlines')
+
+en_titles = open('download_links_en.jsonlines')
+jp_titles = open('download_links_ja.jsonlines')
+
+dl_links_en = {json.loads(l)['info']['name']: json.loads(l) for l in en_titles}
+dl_links_jp = {json.loads(l)['info']['name']: json.loads(l) for l in jp_titles}
+
+
+with open('reids_dl_links_en.jsonlines', 'a') as file: 
+    for title in en_matches:
+        json_str = json.dumps(get_dl_links(title, dl_links_en))
+        file.write(json_str + '\n')
+        file.flush()
+
+
+with open('reids_dl_links_jp.jsonlines', 'a') as file: 
+    for title in jp_matches:
+        json_str = json.dumps(get_dl_links(title, dl_links_jp))
+        file.write(json_str + '\n')
+        file.flush()
+
+
+#print len(en_matches)
+#print len(set(en_matches) & set(dl_links_en.keys()))
+
+#print len(jp_matches)
+#print len(set(jp_matches) & set(dl_links_jp.keys()))
 
 
